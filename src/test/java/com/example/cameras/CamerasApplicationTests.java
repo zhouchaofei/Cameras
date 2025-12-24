79d8a2349ea0b1afc38285c732959e3e892b30eb
zhouchaofei <1479253004@qq.com>
2025-12-24 11:19:21 +0800

    新建项目

---
+++ src/test/java/com/example/cameras/CamerasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cameras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamerasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
